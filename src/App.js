8f3734dd4e732dcaa15317956a4e0e647800284f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-19 22:41:38 -0700

    commit tree 2ae98e86234c56a6112a1f32f6557b14045bbdc8

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
 // create your App component here
+import React, {Component} from 'react'
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            astros: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(resp => resp.json())
+            .then(astros => {
+                this.setState({
+                    astros: astros.people
+                }
+                )
+            }
+            )
+    }
+    
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.astros.map(astro => <li>{astro.name}</li>)}
+                </ul>
+            </div>  
+        )
+    }
+}
+
+export default App
